1190c42cae395891f68c1393ab825e6c5d8e1055
Leonardo Nicolas <leonicolas@gmail.com>
2020-06-02 23:03:54 -0700

    Add check to sobject fields nodes.

--- src/main/java/com/querybuilder/QueryBuilderWindow.java
+++ src/main/java/com/querybuilder/QueryBuilderWindow.java
@@ -1,19 +1,18 @@
 package com.querybuilder;
 
 import com.intellij.openapi.wm.ToolWindow;
-import com.intellij.ui.CheckedTreeNode;
 import com.intellij.ui.treeStructure.Tree;
+import org.scijava.swing.checkboxtree.CheckBoxNodeData;
+import org.scijava.swing.checkboxtree.CheckBoxNodeEditor;
+import org.scijava.swing.checkboxtree.CheckBoxNodeRenderer;
 
 import javax.swing.*;
 import javax.swing.tree.DefaultMutableTreeNode;
 import javax.swing.tree.DefaultTreeModel;
-import javax.swing.tree.TreeModel;
 import javax.swing.tree.TreePath;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-
-import static groovyjarjarantlr.build.ANTLR.root;
 
 public class QueryBuilderWindow {
 
@@ -22,6 +21,16 @@ public class QueryBuilderWindow {
     private Tree sObjectsTree;
 
     public QueryBuilderWindow(ToolWindow toolWindow) {
+        // Initializes checkbox node rerender.
+        final CheckBoxNodeRenderer renderer = new CheckBoxNodeRenderer();
+        sObjectsTree.setCellRenderer(renderer);
+
+        // Initializes checkbox node editor.
+        final CheckBoxNodeEditor editor = new CheckBoxNodeEditor(sObjectsTree);
+        sObjectsTree.setCellEditor(editor);
+        sObjectsTree.setEditable(true);
+
+        // Initializes tree data.
         loadSObjects(new ArrayList<>() {{
             add(new SObject("Account", List.of("Id", "Name", "Custom_Field__c")));
             add(new SObject("Opportunity", List.of("Id", "Name", "Custom_Field__c")));
@@ -52,9 +61,8 @@ public class QueryBuilderWindow {
         parentNode.add(sObjectNode);
 
         for(String field : sObject.fields) {
-            CheckedTreeNode fieldNode = new CheckedTreeNode(field);
-            fieldNode.setChecked(false);
-            sObjectNode.add(fieldNode);
+            CheckBoxNodeData fieldNode = new CheckBoxNodeData(field, false);
+            sObjectNode.add(new DefaultMutableTreeNode(fieldNode));
         }
     }
 
