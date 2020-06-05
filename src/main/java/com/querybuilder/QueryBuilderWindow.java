07e6849e5010114ee6aacb38ed7d05454c09acdb
Leonardo Nicolas <leonicolas@gmail.com>
2020-06-05 09:53:58 -0700

    Support the new SObjectData class.

--- src/main/java/com/querybuilder/QueryBuilderWindow.java
+++ src/main/java/com/querybuilder/QueryBuilderWindow.java
@@ -1,11 +1,8 @@
 package com.querybuilder;
 
-import com.intellij.openapi.wm.ToolWindow;
 import com.intellij.ui.CheckboxTree;
-import com.intellij.ui.CheckboxTreeBase;
 import com.intellij.ui.CheckboxTreeListener;
 import com.intellij.ui.CheckedTreeNode;
-import org.apache.commons.lang.StringUtils;
 import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
@@ -19,43 +16,18 @@ public class QueryBuilderWindow {
     private JPanel panel;
     private JTextArea queryTextArea;
     private CheckboxTree sObjectsTree;
-
-
-    public QueryBuilderWindow(ToolWindow toolWindow) {
-
-//        sObjectsTree.setCellRenderer(new CheckboxTree.CheckboxTreeCellRenderer(false) {
-//            @Override
-//            public void customizeRenderer(JTree tree, Object value, boolean selected, boolean expanded, boolean leaf, int row, boolean hasFocus) {
-//                super.customizeRenderer(tree, value, selected, expanded, leaf, row, hasFocus);
-//                String nodeText = "";
-//                if(value instanceof CheckedTreeNode) {
-//                    nodeText = (String) ((CheckedTreeNode) value).getUserObject();
-//                }
-//                else if(value instanceof DefaultMutableTreeNode) {
-//                    nodeText = (String) ((DefaultMutableTreeNode) value).getUserObject();
-//                }
-//                getTextRenderer().append(nodeText == null ? "" : nodeText);
-//            }
-//        });
-
-        // Initializes tree data.
-        loadSObjects(new ArrayList<>() {{
-            add(new SObject("Account", List.of("Id", "Name", "Custom_Field__c")));
-            add(new SObject("Opportunity", List.of("Id", "Name", "Custom_Field__c")));
-            add(new SObject("User", List.of("Id", "Name", "Custom_Field__c")));
-        }});
-    }
+    private Map<String, Set<String>> selectedFieldsBySObject = new HashMap<>();
 
     public JPanel getContent() {
         return panel;
     }
 
-    public void loadSObjects(List<SObject> sObjects) {
+    public void loadSObjectData(List<SObjectData> sObjectsData) {
         DefaultTreeModel model = (DefaultTreeModel) sObjectsTree.getModel();
         DefaultMutableTreeNode rootNode = (DefaultMutableTreeNode) model.getRoot();
         rootNode.removeAllChildren();
 
-        for(SObject node : sObjects) {
+        for(SObjectData node : sObjectsData) {
             addNode(rootNode, node);
         }
 
@@ -64,19 +36,17 @@ public class QueryBuilderWindow {
         sObjectsTree.expandPath(new TreePath(rootNode));
     }
 
-    private void addNode(DefaultMutableTreeNode parentNode, SObject sObject) {
-        DefaultMutableTreeNode sObjectNode = new DefaultMutableTreeNode(sObject.name);
+    private void addNode(DefaultMutableTreeNode parentNode, SObjectData sObjectData) {
+        DefaultMutableTreeNode sObjectNode = new DefaultMutableTreeNode(sObjectData.getName());
         parentNode.add(sObjectNode);
 
-        for(String fieldName : sObject.fields) {
-            CheckedTreeNode fieldNode = new CheckedTreeNode(fieldName);
+        for(SObjectData.FieldData fieldData : sObjectData.getFields()) {
+            CheckedTreeNode fieldNode = new CheckedTreeNode(fieldData.getName());
             fieldNode.setChecked(false);
             fieldNode.setEnabled(true);
             sObjectNode.add(fieldNode);
         }
     }
-
-    private Map<String, Set<String>> selectedFieldsBySObject = new HashMap<>();
 
     private void createUIComponents() {
         sObjectsTree = new CheckboxTree(new CheckboxTreeCellCustomRenderer(), null);
@@ -111,31 +81,9 @@ public class QueryBuilderWindow {
                 .append(String.join(", ", sObjectData.getValue()))
                 .append("\nFROM ")
                 .append(sObjectData.getKey())
-                .append(";\n");
+                .append(";\n\n");
         }
         queryTextArea.setText(sb.toString());
-    }
-
-    public class SObject {
-
-        private String name;
-        private List<String> fields;
-
-        public SObject(String name, List<String> fields) {
-            this.name = name;
-            if(fields == null) {
-                fields = List.of();
-            }
-            this.fields = Collections.unmodifiableList(fields);
-        }
-
-        public String getName() {
-            return name;
-        }
-
-        public List<String> getFields() {
-            return fields;
-        }
     }
 
     private class CheckboxTreeCellCustomRenderer extends CheckboxTree.CheckboxTreeCellRenderer {
