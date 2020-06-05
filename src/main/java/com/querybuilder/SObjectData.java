6e720b048f08daadc691b5f5c9d038faecda883b
Leonardo Berardino <leonardo@gscloudsolutions.com>
2020-06-05 09:52:21 -0700

    Create SObject data class to map SF metadata.

---
+++ src/main/java/com/querybuilder/SObjectData.java
@@ -0,0 +1,39 @@
+package com.querybuilder;
+
+import java.util.Collections;
+import java.util.List;
+
+public class SObjectData {
+
+    private String name;
+    private List<FieldData> fields;
+
+    public SObjectData(String name, List<FieldData> fields) {
+        this.name = name;
+        if (fields == null) {
+            fields = List.of();
+        }
+        this.fields = Collections.unmodifiableList(fields);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<FieldData> getFields() {
+        return fields;
+    }
+
+    public static class FieldData {
+
+        private String name;
+
+        public FieldData(String name) {
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;
+        }
+    }
+}
